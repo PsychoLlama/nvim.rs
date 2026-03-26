588976157cfd20fe3577e7fd6c738ad63b65ae78
Jesse Gibson <JesseTheGibson@gmail.com>
2026-03-26 05:52:06 -0400

    refactor(ex-cmds2): compress trivial multi-line accessors to one-liners (Phase 2)

--- src/nvim/ex_cmds2.c
+++ src/nvim/ex_cmds2.c
@@ -58,9 +58,7 @@ extern int rs_valid_tabpage(tabpage_T *tpc);
 
 #include "ex_cmds2.c.generated.h"
 
-// =============================================================================
 // Static assertions for constants used by Rust
-// =============================================================================
 
 _Static_assert(CCGD_AW == 1, "CCGD_AW mismatch");
 _Static_assert(CCGD_MULTWIN == 2, "CCGD_MULTWIN mismatch");
@@ -80,29 +78,15 @@ _Static_assert(VIM_ALL == 5, "VIM_ALL mismatch");
 _Static_assert(VIM_DISCARDALL == 6, "VIM_DISCARDALL mismatch");
 
 
-// =============================================================================
 // C accessor functions for Rust (script host)
-// =============================================================================
 
-char *nvim_ex2_eap_get_arg(exarg_T *eap)
-{
-  return eap->arg;
-}
+char *nvim_ex2_eap_get_arg(exarg_T *eap) { return eap->arg; }
 
-linenr_T nvim_ex2_eap_get_line1(exarg_T *eap)
-{
-  return eap->line1;
-}
+linenr_T nvim_ex2_eap_get_line1(exarg_T *eap) { return eap->line1; }
 
-linenr_T nvim_ex2_eap_get_line2(exarg_T *eap)
-{
-  return eap->line2;
-}
+linenr_T nvim_ex2_eap_get_line2(exarg_T *eap) { return eap->line2; }
 
-int nvim_ex2_eap_get_skip(exarg_T *eap)
-{
-  return eap->skip;
-}
+int nvim_ex2_eap_get_skip(exarg_T *eap) { return eap->skip; }
 
 void nvim_ex2_eval_call_provider(char *provider, char *method, list_T *arguments, bool discard)
 {
@@ -110,74 +94,33 @@ void nvim_ex2_eval_call_provider(char *provider, char *method, list_T *arguments
   rs_eval_call_provider(provider, method, arguments, discard, &rettv);
 }
 
-// =============================================================================
 // C accessor functions for Rust (autowrite / buffer check)
-// =============================================================================
 
-const char *nvim_ex2_buf_get_ffname(buf_T *buf)
-{
-  return buf->b_ffname;
-}
+const char *nvim_ex2_buf_get_ffname(buf_T *buf) { return buf->b_ffname; }
 
-const char *nvim_ex2_buf_get_fname(buf_T *buf)
-{
-  return buf->b_fname;
-}
+const char *nvim_ex2_buf_get_fname(buf_T *buf) { return buf->b_fname; }
 
-int nvim_ex2_buf_get_b_p_ro(buf_T *buf)
-{
-  return buf->b_p_ro;
-}
+int nvim_ex2_buf_get_b_p_ro(buf_T *buf) { return buf->b_p_ro; }
 
-int nvim_ex2_buf_get_nwindows(buf_T *buf)
-{
-  return buf->b_nwindows;
-}
+int nvim_ex2_buf_get_nwindows(buf_T *buf) { return buf->b_nwindows; }
 
-linenr_T nvim_ex2_buf_get_ml_line_count(buf_T *buf)
-{
-  return buf->b_ml.ml_line_count;
-}
+linenr_T nvim_ex2_buf_get_ml_line_count(buf_T *buf) { return buf->b_ml.ml_line_count; }
 
-buf_T *nvim_ex2_get_firstbuf(void)
-{
-  return firstbuf;
-}
+buf_T *nvim_ex2_get_firstbuf(void) { return firstbuf; }
 
-buf_T *nvim_ex2_buf_next(buf_T *buf)
-{
-  return buf->b_next;
-}
+buf_T *nvim_ex2_buf_next(buf_T *buf) { return buf->b_next; }
 
-bool nvim_ex2_get_p_aw(void)
-{
-  return p_aw;
-}
+bool nvim_ex2_get_p_aw(void) { return p_aw; }
 
-bool nvim_ex2_get_p_awa(void)
-{
-  return p_awa;
-}
+bool nvim_ex2_get_p_awa(void) { return p_awa; }
 
-bool nvim_ex2_get_p_write(void)
-{
-  return p_write;
-}
+bool nvim_ex2_get_p_write(void) { return p_write; }
 
-bool nvim_ex2_get_p_confirm(void)
-{
-  return p_confirm;
-}
+bool nvim_ex2_get_p_confirm(void) { return p_confirm; }
 
-buf_T *nvim_ex2_get_curbuf(void)
-{
-  return curbuf;
-}
+buf_T *nvim_ex2_get_curbuf(void) { return curbuf; }
 
-uint32_t nvim_ex2_get_cmod_flags(void)
-{
-  return cmdmod.cmod_flags;
-}
+uint32_t nvim_ex2_get_cmod_flags(void) { return cmdmod.cmod_flags; }
 
 bufref_T *nvim_ex2_bufref_create(buf_T *buf)
 {
@@ -186,44 +129,21 @@ bufref_T *nvim_ex2_bufref_create(buf_T *buf)
   return br;
 }
 
-bool nvim_ex2_bufref_valid(bufref_T *br)
-{
-  return bufref_valid(br);
-}
+bool nvim_ex2_bufref_valid(bufref_T *br) { return bufref_valid(br); }
 
-void nvim_ex2_bufref_free(bufref_T *br)
-{
-  xfree(br);
-}
+void nvim_ex2_bufref_free(bufref_T *br) { xfree(br); }
 
-bool nvim_ex2_bufIsChanged(buf_T *buf)
-{
-  return bufIsChanged(buf);
-}
+bool nvim_ex2_bufIsChanged(buf_T *buf) { return bufIsChanged(buf); }
 
-bool nvim_ex2_bt_dontwrite(buf_T *buf)
-{
-  return bt_dontwrite(buf);
-}
+bool nvim_ex2_bt_dontwrite(buf_T *buf) { return bt_dontwrite(buf); }
 
-bool nvim_ex2_buf_hide(buf_T *buf)
-{
-  return buf_hide(buf);
-}
+bool nvim_ex2_buf_hide(buf_T *buf) { return buf_hide(buf); }
 
-const char *nvim_ex2_gettext(const char *s)
-{
-  return _(s);
-}
+const char *nvim_ex2_gettext(const char *s) { return _(s); }
 
-// =============================================================================
 // C accessor functions for Rust (dialog)
-// =============================================================================
 
-int nvim_ex2_buf_get_fnum(buf_T *buf)
-{
-  return buf->b_fnum;
-}
+int nvim_ex2_buf_get_fnum(buf_T *buf) { return buf->b_fnum; }
 
 int nvim_ex2_check_overwrite(buf_T *buf, const char *fname, const char *ffname)
 {
@@ -240,47 +160,24 @@ void nvim_ex2_buf_clear_names(buf_T *buf)
   XFREE_CLEAR(buf->b_sfname);
 }
 
-void nvim_ex2_buf_set_fname_null(buf_T *buf)
-{
-  buf->b_fname = NULL;
-}
+void nvim_ex2_buf_set_fname_null(buf_T *buf) { buf->b_fname = NULL; }
 
-// =============================================================================
 // C accessor functions for Rust (check_changed_any / ex_checktime)
-// =============================================================================
 
 _Static_assert(DOBUF_UNLOAD == 2, "DOBUF_UNLOAD mismatch");
 _Static_assert(DOBUF_GOTO == 0, "DOBUF_GOTO mismatch");
 
-win_T *nvim_ex2_get_firstwin(void)
-{
-  return firstwin;
-}
+win_T *nvim_ex2_get_firstwin(void) { return firstwin; }
 
-win_T *nvim_ex2_win_next(win_T *win)
-{
-  return win->w_next;
-}
+win_T *nvim_ex2_win_next(win_T *win) { return win->w_next; }
 
-buf_T *nvim_ex2_win_get_buffer(win_T *win)
-{
-  return win->w_buffer;
-}
+buf_T *nvim_ex2_win_get_buffer(win_T *win) { return win->w_buffer; }
 
-tabpage_T *nvim_ex2_get_curtab(void)
-{
-  return curtab;
-}
+tabpage_T *nvim_ex2_get_curtab(void) { return curtab; }
 
-tabpage_T *nvim_ex2_get_first_tabpage(void)
-{
-  return first_tabpage;
-}
+tabpage_T *nvim_ex2_get_first_tabpage(void) { return first_tabpage; }
 
-tabpage_T *nvim_ex2_tp_next(tabpage_T *tp)
-{
-  return tp->tp_next;
-}
+tabpage_T *nvim_ex2_tp_next(tabpage_T *tp) { return tp->tp_next; }
 
 win_T *nvim_ex2_tp_firstwin(tabpage_T *tp)
 {
@@ -290,50 +187,23 @@ win_T *nvim_ex2_tp_firstwin(tabpage_T *tp)
   return tp->tp_firstwin;
 }
 
-int nvim_ex2_get_vgetc_busy(void)
-{
-  return vgetc_busy;
-}
+int nvim_ex2_get_vgetc_busy(void) { return vgetc_busy; }
 
-void nvim_ex2_set_msg_row(int val)
-{
-  msg_row = val;
-}
+void nvim_ex2_set_msg_row(int val) { msg_row = val; }
 
-void nvim_ex2_set_msg_col(int val)
-{
-  msg_col = val;
-}
+void nvim_ex2_set_msg_col(int val) { msg_col = val; }
 
-void nvim_ex2_set_msg_didout(bool val)
-{
-  msg_didout = val;
-}
+void nvim_ex2_set_msg_didout(bool val) { msg_didout = val; }
 
-bool nvim_ex2_get_msg_didany(void)
-{
-  return msg_didany;
-}
+bool nvim_ex2_get_msg_didany(void) { return msg_didany; }
 
-int nvim_ex2_get_cmdline_row(void)
-{
-  return cmdline_row;
-}
+int nvim_ex2_get_cmdline_row(void) { return cmdline_row; }
 
-int nvim_ex2_get_no_wait_return(void)
-{
-  return no_wait_return;
-}
+int nvim_ex2_get_no_wait_return(void) { return no_wait_return; }
 
-void nvim_ex2_set_no_wait_return(int val)
-{
-  no_wait_return = val;
-}
+void nvim_ex2_set_no_wait_return(int val) { no_wait_return = val; }
 
-void nvim_ex2_set_exiting(bool val)
-{
-  exiting = val;
-}
+void nvim_ex2_set_exiting(bool val) { exiting = val; }
 
 bool nvim_ex2_buf_has_running_job(buf_T *buf)
 {
@@ -341,165 +211,78 @@ bool nvim_ex2_buf_has_running_job(buf_T *buf)
 }
 
 
-int nvim_ex2_eap_get_addr_count(exarg_T *eap)
-{
-  return eap->addr_count;
-}
+int nvim_ex2_eap_get_addr_count(exarg_T *eap) { return eap->addr_count; }
 
-int nvim_ex2_get_no_check_timestamps(void)
-{
-  return no_check_timestamps;
-}
+int nvim_ex2_get_no_check_timestamps(void) { return no_check_timestamps; }
 
-void nvim_ex2_set_no_check_timestamps(int val)
-{
-  no_check_timestamps = val;
-}
+void nvim_ex2_set_no_check_timestamps(int val) { no_check_timestamps = val; }
 
 
-// =============================================================================
 // C accessor functions for Rust (compiler)
-// =============================================================================
 
 _Static_assert(DIP_ALL == 0x01, "DIP_ALL mismatch");
 
-int nvim_ex2_eap_get_forceit(exarg_T *eap)
-{
-  return eap->forceit;
-}
+int nvim_ex2_eap_get_forceit(exarg_T *eap) { return eap->forceit; }
 
 
-// =============================================================================
 // C accessor functions for Rust (drop)
-// =============================================================================
 
 _Static_assert(ML_EMPTY == 0x01, "ML_EMPTY mismatch");
 _Static_assert(DOCMD_VERBOSE == 0x01, "DOCMD_VERBOSE mismatch");
 _Static_assert(DOCMD_NOWAIT == 0x02, "DOCMD_NOWAIT mismatch");
 
-int nvim_ex2_get_argcount(void)
-{
-  return ARGCOUNT;
-}
+int nvim_ex2_get_argcount(void) { return ARGCOUNT; }
 
-int nvim_ex2_get_arglist_fnum(int idx)
-{
-  return ARGLIST[idx].ae_fnum;
-}
+int nvim_ex2_get_arglist_fnum(int idx) { return ARGLIST[idx].ae_fnum; }
 
-win_T *nvim_ex2_get_curwin(void)
-{
-  return curwin;
-}
+win_T *nvim_ex2_get_curwin(void) { return curwin; }
 
-int nvim_ex2_curwin_get_arg_idx(void)
-{
-  return curwin->w_arg_idx;
-}
+int nvim_ex2_curwin_get_arg_idx(void) { return curwin->w_arg_idx; }
 
-void nvim_ex2_curwin_set_arg_idx(int val)
-{
-  curwin->w_arg_idx = val;
-}
+void nvim_ex2_curwin_set_arg_idx(int val) { curwin->w_arg_idx = val; }
 
-int nvim_ex2_curbuf_get_b_p_ar(void)
-{
-  return curbuf->b_p_ar;
-}
+int nvim_ex2_curbuf_get_b_p_ar(void) { return curbuf->b_p_ar; }
 
-void nvim_ex2_curbuf_set_b_p_ar(int val)
-{
-  curbuf->b_p_ar = val;
-}
+void nvim_ex2_curbuf_set_b_p_ar(int val) { curbuf->b_p_ar = val; }
 
-int nvim_ex2_curbuf_get_ml_flags(void)
-{
-  return curbuf->b_ml.ml_flags;
-}
+int nvim_ex2_curbuf_get_ml_flags(void) { return curbuf->b_ml.ml_flags; }
 
-char *nvim_ex2_eap_get_do_ecmd_cmd(exarg_T *eap)
-{
-  return eap->do_ecmd_cmd;
-}
+char *nvim_ex2_eap_get_do_ecmd_cmd(exarg_T *eap) { return eap->do_ecmd_cmd; }
 
 
-void nvim_ex2_clear_swapcommand(void)
-{
-  set_vim_var_string(VV_SWAPCOMMAND, NULL, -1);
-}
+void nvim_ex2_clear_swapcommand(void) { set_vim_var_string(VV_SWAPCOMMAND, NULL, -1); }
 
-void nvim_ex2_buf_check_timestamp_curbuf(void)
-{
-  (void)buf_check_timestamp(curbuf);
-}
+void nvim_ex2_buf_check_timestamp_curbuf(void) { (void)buf_check_timestamp(curbuf); }
 
-int nvim_ex2_get_emsg_off(void)
-{
-  return emsg_off;
-}
+int nvim_ex2_get_emsg_off(void) { return emsg_off; }
 
-void nvim_ex2_set_emsg_off(int val)
-{
-  emsg_off = val;
-}
+void nvim_ex2_set_emsg_off(int val) { emsg_off = val; }
 
-int nvim_ex2_get_cmod_tab(void)
-{
-  return cmdmod.cmod_tab;
-}
+int nvim_ex2_get_cmod_tab(void) { return cmdmod.cmod_tab; }
 
-void nvim_ex2_set_cmod_tab(int val)
-{
-  cmdmod.cmod_tab = val;
-}
+void nvim_ex2_set_cmod_tab(int val) { cmdmod.cmod_tab = val; }
 
-void nvim_ex2_eap_set_cmdidx(exarg_T *eap, int val)
-{
-  eap->cmdidx = (cmdidx_T)val;
-}
+void nvim_ex2_eap_set_cmdidx(exarg_T *eap, int val) { eap->cmdidx = (cmdidx_T)val; }
 
-void nvim_ex2_eap_set_cmd0(exarg_T *eap, int ch)
-{
-  eap->cmd[0] = (char)ch;
-}
+void nvim_ex2_eap_set_cmd0(exarg_T *eap, int ch) { eap->cmd[0] = (char)ch; }
 
-int nvim_ex2_get_cmd_sfirst(void)
-{
-  return (int)CMD_sfirst;
-}
+int nvim_ex2_get_cmd_sfirst(void) { return (int)CMD_sfirst; }
 
-int nvim_ex2_get_cmd_first(void)
-{
-  return (int)CMD_first;
-}
+int nvim_ex2_get_cmd_first(void) { return (int)CMD_first; }
 
-// =============================================================================
 // C accessor functions for Rust (listdo)
-// =============================================================================
 
 _Static_assert(BF_SYN_SET == 0x200, "BF_SYN_SET mismatch");
 _Static_assert(FORWARD == 1, "FORWARD mismatch");
 _Static_assert(DOBUF_FIRST == 1, "DOBUF_FIRST mismatch");
 
-int nvim_ex2_eap_get_cmdidx(exarg_T *eap)
-{
-  return (int)eap->cmdidx;
-}
+int nvim_ex2_eap_get_cmdidx(exarg_T *eap) { return (int)eap->cmdidx; }
 
-void *nvim_ex2_eap_get_ea_getline(exarg_T *eap)
-{
-  return (void *)eap->ea_getline;
-}
+void *nvim_ex2_eap_get_ea_getline(exarg_T *eap) { return (void *)eap->ea_getline; }
 
-void *nvim_ex2_eap_get_cookie(exarg_T *eap)
-{
-  return eap->cookie;
-}
+void *nvim_ex2_eap_get_cookie(exarg_T *eap) { return eap->cookie; }
 
-void nvim_ex2_eap_set_line2(exarg_T *eap, linenr_T val)
-{
-  eap->line2 = val;
-}
+void nvim_ex2_eap_set_line2(exarg_T *eap, linenr_T val) { eap->line2 = val; }
 
 int nvim_ex2_get_cmd_windo(void) { return (int)CMD_windo; }
 int nvim_ex2_get_cmd_tabdo(void) { return (int)CMD_tabdo; }
@@ -510,103 +293,46 @@ int nvim_ex2_get_cmd_ldo(void) { return (int)CMD_ldo; }
 int nvim_ex2_get_cmd_cfdo(void) { return (int)CMD_cfdo; }
 int nvim_ex2_get_cmd_lfdo(void) { return (int)CMD_lfdo; }
 
-bool nvim_ex2_get_curwin_w_p_wfb(void)
-{
-  return curwin->w_p_wfb;
-}
+bool nvim_ex2_get_curwin_w_p_wfb(void) { return curwin->w_p_wfb; }
 
-win_T *nvim_ex2_get_prevwin(void)
-{
-  return prevwin;
-}
+win_T *nvim_ex2_get_prevwin(void) { return prevwin; }
 
-bool nvim_ex2_win_get_w_p_wfb(win_T *win)
-{
-  return win->w_p_wfb;
-}
+bool nvim_ex2_win_get_w_p_wfb(win_T *win) { return win->w_p_wfb; }
 
 
-int nvim_ex2_win_split(void)
-{
-  return win_split(0, 0);
-}
+int nvim_ex2_win_split(void) { return win_split(0, 0); }
 
-void nvim_ex2_emsg_winfixbuf(void)
-{
-  emsg(_(e_winfixbuf_cannot_go_to_buffer));
-}
+void nvim_ex2_emsg_winfixbuf(void) { emsg(_(e_winfixbuf_cannot_go_to_buffer)); }
 
-void nvim_ex2_inc_msg_listdo_overwrite(void)
-{
-  msg_listdo_overwrite++;
-}
+void nvim_ex2_inc_msg_listdo_overwrite(void) { msg_listdo_overwrite++; }
 
-void nvim_ex2_dec_msg_listdo_overwrite(void)
-{
-  msg_listdo_overwrite--;
-}
+void nvim_ex2_dec_msg_listdo_overwrite(void) { msg_listdo_overwrite--; }
 
-char *nvim_ex2_au_event_disable_syntax(void)
-{
-  return au_event_disable(",Syntax");
-}
+char *nvim_ex2_au_event_disable_syntax(void) { return au_event_disable(",Syntax"); }
 
 
-void nvim_ex2_buf_clear_bf_syn_set(buf_T *buf)
-{
-  buf->b_flags &= ~BF_SYN_SET;
-}
+void nvim_ex2_buf_clear_bf_syn_set(buf_T *buf) { buf->b_flags &= ~BF_SYN_SET; }
 
-bool nvim_ex2_buf_get_b_p_bl(buf_T *buf)
-{
-  return buf->b_p_bl;
-}
+bool nvim_ex2_buf_get_b_p_bl(buf_T *buf) { return buf->b_p_bl; }
 
-bool nvim_ex2_get_got_int(void)
-{
-  return got_int;
-}
+bool nvim_ex2_get_got_int(void) { return got_int; }
 
-void nvim_ex2_set_listcmd_busy(bool val)
-{
-  listcmd_busy = val;
-}
+void nvim_ex2_set_listcmd_busy(bool val) { listcmd_busy = val; }
 
-void nvim_ex2_setpcmark(void)
-{
-  setpcmark();
-}
+void nvim_ex2_setpcmark(void) { setpcmark(); }
 
-bool nvim_ex2_editing_arg_idx(void)
-{
-  return editing_arg_idx(curwin);
-}
+bool nvim_ex2_editing_arg_idx(void) { return editing_arg_idx(curwin); }
 
 
-bool nvim_ex2_win_get_w_floating(win_T *win)
-{
-  return win->w_floating;
-}
+bool nvim_ex2_win_get_w_floating(win_T *win) { return win->w_floating; }
 
-bool nvim_ex2_win_get_w_config_hide(win_T *win)
-{
-  return win->w_config.hide;
-}
+bool nvim_ex2_win_get_w_config_hide(win_T *win) { return win->w_config.hide; }
 
-bool nvim_ex2_win_get_w_config_focusable(win_T *win)
-{
-  return win->w_config.focusable;
-}
+bool nvim_ex2_win_get_w_config_focusable(win_T *win) { return win->w_config.focusable; }
 
-void nvim_ex2_validate_cursor(void)
-{
-  validate_cursor(curwin);
-}
+void nvim_ex2_validate_cursor(void) { validate_cursor(curwin); }
 
-bool nvim_ex2_curwin_get_w_p_scb(void)
-{
-  return curwin->w_p_scb;
-}
+bool nvim_ex2_curwin_get_w_p_scb(void) { return curwin->w_p_scb; }
 
 
 /// Restore syntax autocommands after listdo.
